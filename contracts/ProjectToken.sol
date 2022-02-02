cc4bc1a558018706deb493b15f6a5d7f07d332ab
Spahr Michael <spahr.michael@protonmail.com>
2022-02-02 21:53:11 +0100

    add Token smart contract

---
+++ contracts/ProjectToken.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title ProjectToken
+ * @author Michael Spahr
+ */
+contract ProjectToken is ERC20 {
+    constructor() public ERC20("Project Token", "PJTK") {
+        _mint(msg.sender, 1 * 10**6);
+    }
+}
