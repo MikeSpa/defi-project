e4a843522b77ef243dbc02920f1fca9829a2617f
Spahr Michael <spahr.michael@protonmail.com>
2022-02-03 22:57:20 +0100

    add deposit to aave script

---
+++ scripts/deposit_to_aave.py
@@ -0,0 +1,38 @@
+from scripts.helpful_scripts import (
+    get_account,
+    get_weth,
+    approve_erc20,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    FORKED_LOCAL_ENVIRNOMENT,
+)
+from brownie import config, network, interface
+from web3 import Web3
+
+AMOUNT = Web3.toWei(0.1, "ether")
+
+
+def get_lending_pool():
+    # return the address of the lending pool
+    print("get_lending_pool")
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+
+
+def main():
+    print("## deposit_to_aave")
+    account = get_account()
+    weth_address = config["networks"][network.show_active()]["weth_token"]
+    get_weth()
+
+    lending_pool = get_lending_pool()
+    approve_erc20(weth_address, lending_pool.address, AMOUNT, account)
+    print("Depositing...")
+    deposit_tx = lending_pool.deposit(
+        weth_address, AMOUNT, account.address, 0, {"from": account}
+    )
+    deposit_tx.wait(1)
+    print("Deposited")
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,4 @@
-from brownie import (
-    network,
-    accounts,
-    config,
-)
+from brownie import network, accounts, config, interface
 from web3 import Web3
 
 FORKED_LOCAL_ENVIRNOMENT = ["mainnet-fork", "mainnet-fork2"]
@@ -12,6 +8,7 @@ CENT = Web3.toWei(100, "ether")
 
 
 def get_account(index=None, id=None):
+    print("#get_account")
     if index:
         return accounts[index]
     if id:
@@ -24,3 +21,31 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+def get_weth(amount=0.1):
+    print(f"#get_weth, amt={amount}")
+    account = get_account()
+    weth = interface.IWETH(config["networks"][network.show_active()]["weth_token"])
+    deposit_tx = weth.deposit({"from": account, "value": amount * 10 ** 18})
+    deposit_tx.wait(1)
+    print(f"Received {amount} WETH")
+    return deposit_tx
+
+
+def get_eth(amount=0.1):
+    print(f"#get_eth, amt={amount}")
+    account = get_account()
+    weth = interface.IWETH(config["networks"][network.show_active()]["weth_token"])
+    withdraw_tx = weth.withdraw(amount * 10 ** 18, {"from": account})
+    withdraw_tx.wait(1)
+    print(f"Received {amount} ETH")
+    return withdraw_tx
+
+
+def approve_erc20(token_address, spender, amount, account):
+    print(f"#approve_erc20, {amount} {token_address}")
+    erc20 = interface.IERC20(token_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
