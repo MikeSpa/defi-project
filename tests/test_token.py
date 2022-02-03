b0ad68641655a19d46f001550327f63e4bf599c9
Spahr Michael <spahr.michael@protonmail.com>
2022-02-03 20:38:28 +0100

    add test for token contract

---
+++ tests/test_token.py
@@ -0,0 +1,22 @@
+from brownie import network, config, ProjectToken
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+)
+import pytest
+from scripts.deploy import deploy_token
+
+MM = 1_000_000_000_000_000_000_000_000
+
+
+def test_issue_tokens():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token = deploy_token()
+
+    starting_balance_account = token.balanceOf(account.address)
+    starting_balance_contract = token.balanceOf(token.address)
+
+    assert starting_balance_contract == MM
+    assert starting_balance_account == 0
