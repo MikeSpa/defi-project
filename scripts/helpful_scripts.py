49dcafb81157e1d84011373e6a6898d6bef85f0a
Spahr Michael <spahr.michael@protonmail.com>
2022-02-04 23:17:20 +0100

    Add MockAggregator

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import network, accounts, config, interface
+from brownie import network, accounts, config, interface, MockV3Aggregator, Contract
 from web3 import Web3
 
 FORKED_LOCAL_ENVIRNOMENT = ["mainnet-fork", "mainnet-fork2"]
@@ -59,6 +59,50 @@ def get_asset_price(price_feed_address):
     return float(converted_latest_price)
 
 
+contract_to_mock = {
+    "dai_eth_price_feed": MockV3Aggregator,
+}
+
+
+def get_contract(contract_name):
+    """
+    This function will grab the contract addresses
+    from the brownie config if defined,
+    otherwise it will deploy a mock version of that
+    contract and return taht mock contract
+        Args:
+            contract_name (string)
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
+    """
+    print("#get_contract")
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+def deploy_mocks():
+    account = get_account()
+    print(f"### The active netwok is {network.show_active()}")
+    print("### Deploying Mocks...")
+    # MockV3Aggregator is a list of all MockV3Agg we've deployed
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            8, Web3.toWei(123_456_000_000, "ether"), {"from": account}
+        )
+        print("MockV3Aggregator deployed")
+
+
 def main():
-    get_asset_price(config["networks"][network.show_active()]["dai_eth_price_feed"])
+    get_asset_price(get_contract("dai_eth_price_feed"))
     pass
