4bf30ff1ceed9b6a524ecaeeae2a517ef546519c
Spahr Michael <spahr.michael@protonmail.com>
2022-02-10 17:04:55 +0100

    remove unneeded stuff

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DAppProvider, Kovan } from '@usedapp/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <DAppProvider config={{
+      networks: [Kovan],
+      notifications: { //notifications
+        expirationPeriod: 1000, //notif lasts 1sec
+        checkInterval: 1000 //check every second
+      }
+    }}>
+      <div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          DeFi project
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/MikeSpa/defi-project"
         >
-          Learn React
+          Project Github
         </a>
-      </header>
-    </div>
+      </div>
+    </DAppProvider>
+
   );
 }
 
