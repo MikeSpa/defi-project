b98e4b2b556826c99663bd5850fb477e253be1af
Spahr Michael <spahr.michael@protonmail.com>
2022-02-12 15:15:38 +0100

    copy deployment info from back end to front end

--- scripts/deploy_staking_contract.py
+++ scripts/deploy_staking_contract.py
@@ -8,8 +8,13 @@ from scripts.helpful_scripts import (
 )
 from brownie import ProjectToken, StakingContract, config, network
 
+import yaml
+import json
+import os
+import shutil
 
-def deploy_staking_contract_and_project_token():
+
+def deploy_staking_contract_and_project_token(front_end_update=False):
     account = get_account()
     project_token = ProjectToken.deploy(
         {"from": account},
@@ -31,6 +36,8 @@ def deploy_staking_contract_and_project_token():
         weth_token: get_contract("eth_usd_price_feed"),
     }
     add_allowed_tokens(staking_contract, pricefeed_of_token, account)
+    if front_end_update:
+        update_front_end()
     return staking_contract, project_token, weth_token
 
 
@@ -72,23 +79,33 @@ def deploy_and_stake(amt=POINT_ONE):
     return staking_contract, project_token, weth_token
 
 
+def update_front_end():
+    # Send the build folder
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+
+    # Sending the front end our config in JSON format
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
+
+
+def copy_folders_to_front_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
+
 def main():
     account = get_account()
+    front_end_update = True
     (
         staking_contract,
         project_token,
         weth_token,
-    ) = deploy_staking_contract_and_project_token()
-    weth_token_address = get_contract("weth_token")
-    stake_and_approve_token(staking_contract, weth_token_address, POINT_ONE, account)
-    balacne = weth_token_address.balanceOf(account.address)
-    print(balacne)
-    balacne = weth_token_address.balanceOf(staking_contract.address)
-    print(balacne)
+    ) = deploy_staking_contract_and_project_token(front_end_update)
+    # weth_token_address = get_contract("weth_token")
+    # stake_and_approve_token(staking_contract, weth_token_address, POINT_ONE, account)
 
-    unstake_token(staking_contract, weth_token_address, account)
-
-    balacne = weth_token_address.balanceOf(account.address)
-    print(balacne)
-    balacne = weth_token_address.balanceOf(staking_contract.address)
-    print(balacne)
+    # unstake_token(staking_contract, weth_token_address, account)
