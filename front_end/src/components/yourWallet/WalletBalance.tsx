d07ae22fb139d58e91469ae9885fa6ac41dff4bd
Spahr Michael <spahr.michael@protonmail.com>
2022-02-17 17:16:37 +0100

    add decimal in wallet balance display

--- front_end/src/components/yourWallet/WalletBalance.tsx
+++ front_end/src/components/yourWallet/WalletBalance.tsx
@@ -12,7 +12,9 @@ export const WalletBalance = ({ token }: WalletBalanceProps) => {
     const { image, address, name } = token
     const { account } = useEthers()
     const tokenBalance = useTokenBalance(address, account)
-    const formattedTokenBalance: number = tokenBalance ? parseInt(formatUnits(tokenBalance, 18)) : 0
+    const formattedTokenBalance = tokenBalance ? parseInt(formatUnits(tokenBalance, 17)) / 10 : 0
+    console.log(formattedTokenBalance)
+    // formattedTokenBalance = 
     return (<BalanceMsg
         label={'Your un-staked ' + name + ' balance:'}
         tokenImgSrc={image}
