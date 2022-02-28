0c39d2e40a4c045d5b83adb6e4c3893973968eed
Spahr Michael <spahr.michael@protonmail.com>
2022-02-28 16:39:10 +0100

    refactoring

--- tests/test_lending_protocol_mock.py
+++ tests/test_lending_protocol_mock.py
@@ -18,25 +18,25 @@ def test_deploy_mock():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing!")
     account = get_account()
-    mock_aave_lending = deploy_aave_lending_contract()
+    mock_lending = deploy_aave_lending_contract()
 
     weth_token = get_contract("weth_token")
 
-    assert mock_aave_lending.stakingBalancePerToken(weth_token) == 0
+    assert mock_lending.stakingBalancePerToken(weth_token) == 0
 
 
 def test_deposit(amt=POINT_ONE):
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing!")
     account = get_account()
-    mock_aave_lending = deploy_aave_lending_contract()
+    mock_lending = deploy_aave_lending_contract()
 
     weth_token = get_contract("weth_token")
     initial_acc_balance = weth_token.balanceOf(account)
 
-    approve_erc20(weth_token, mock_aave_lending, amt, account)
-    deposit_aave(mock_aave_lending, weth_token, amt, account)
-    assert mock_aave_lending.stakingBalancePerToken(weth_token) == amt
+    approve_erc20(weth_token, mock_lending, amt, account)
+    deposit_aave(mock_lending, weth_token, amt, account)
+    assert mock_lending.stakingBalancePerToken(weth_token) == amt
     assert weth_token.balanceOf(account) == initial_acc_balance - amt
 
 
@@ -45,20 +45,20 @@ def test_withdraw(amt=POINT_ONE):
         pytest.skip("Only for local testing!")
     account = get_account()
     acc2 = get_account(index=1)
-    mock_aave_lending = deploy_aave_lending_contract()
+    mock_lending = deploy_aave_lending_contract()
 
     weth_token = get_contract("weth_token")
     initial_acc_balance = weth_token.balanceOf(account)
 
     # Mock already as the first deposit
-    assert mock_aave_lending.stakingBalancePerToken(weth_token) == amt
+    assert mock_lending.stakingBalancePerToken(weth_token) == amt
 
-    withdraw_aave(mock_aave_lending, weth_token, amt / 2, account)
+    withdraw_aave(mock_lending, weth_token, amt / 2, account)
 
-    assert mock_aave_lending.stakingBalancePerToken(weth_token) == amt / 2
+    assert mock_lending.stakingBalancePerToken(weth_token) == amt / 2
     assert weth_token.balanceOf(account) == initial_acc_balance + amt / 2
 
-    withdraw_aave(mock_aave_lending, weth_token, amt / 2, account)
+    withdraw_aave(mock_lending, weth_token, amt / 2, account)
 
-    assert mock_aave_lending.stakingBalancePerToken(weth_token) == 0
+    assert mock_lending.stakingBalancePerToken(weth_token) == 0
     assert weth_token.balanceOf(account) == initial_acc_balance + amt
