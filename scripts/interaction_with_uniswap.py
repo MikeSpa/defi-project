1946dea90a0debe08b10c8667b38eba8f0c4c8bd
Spahr Michael <spahr.michael@protonmail.com>
2022-03-04 13:25:09 +0100

    add UniswapV2Router02 interface

--- scripts/interaction_with_uniswap.py
+++ scripts/interaction_with_uniswap.py
@@ -9,12 +9,25 @@ from scripts.helpful_scripts import (
 from brownie import config, network, interface, ProjectToken
 from web3 import Web3
 
+DAI_PJTK_PAIR = "0xA4f3f3D34Bd9AEB1901ade030CBCA978878dbE8e"
+
 
 def get_factory():
     # return the address of UniswapV2Factory
     return interface.IUniswapV2Factory(
         config["networks"][network.show_active()]["UniswapV2Factory"]
     )
+
+
+def get_router():
+    # return the address of UniswapV2Factory
+    return interface.IUniswapV2Router02(
+        config["networks"][network.show_active()]["UniswapV2Router02"]
+    )
+
+
+def get_pair_x():
+    return interface.IUniswapV2Pair(DAI_PJTK_PAIR)
 
 
 def get_pair(token0, token1):
@@ -36,6 +49,59 @@ def create_pair(token0, token1):
     return pair
 
 
+def add_liquidity(
+    tokenA,
+    tokenB,
+    amount_a_desired,
+    amount_b_desired,
+    amount_a_min=None,
+    amount_b_min=None,
+    to=get_account(),
+):
+    # returns amountA, amountB, liquidity
+    account = get_account()
+    router = get_router()
+    if not amount_a_min:
+        amount_a_min = amount_a_desired * 99 / 100
+    if not amount_a_min:
+        amount_a_min = amount_a_desired * 99 / 100
+
+    approve_erc20(tokenA, router, amount_a_desired, account)
+    erc20 = interface.IERC20(tokenB)
+    tx = erc20.approve(router, amount_b_desired, {"from": account})
+    timestamp = tx.timestamp
+    print(timestamp)
+    pair = get_pair_x()
+    resA, resB, _ = pair.getReserves()
+    print(f"reservesA: {resA}")
+    print(f"reservesB: {resB}")
+    # timestamp = tx.timestamp
+
+    # Fail with error 'ds-math-sub-underflow'
+    tx = router.addLiquidity(
+        tokenA,
+        tokenB,
+        amount_a_desired,
+        amount_b_desired,
+        amount_a_min,
+        amount_b_min,
+        account,
+        timestamp + 100,
+        # 2 ** 255,
+        {"from": account, "gas_limit": 1_000_000, "allow_revert": True},
+    )
+    print(tx)
+    A, B, liquidity = tx.return_value
+    print(A)
+    print(B)
+    print(liquidity)
+
+    resA, resB, _ = pair.getReserves()
+    print(f"reservesA: {resA}")
+    print(f"reservesB: {resB}")
+    return A, B, liquidity
+
+
 def main():
     print("## Uniswap")
     account = get_account()
@@ -43,5 +109,7 @@ def main():
     weth = get_contract("weth_token")
     pjtk = ProjectToken[-1]
 
-    pair = create_pair(dai, pjtk)
-    print(pair)
+    # pair = create_pair(dai, pjtk)
+    # print(pair)
+
+    a, b, l = add_liquidity(dai, pjtk, 10, 10)
