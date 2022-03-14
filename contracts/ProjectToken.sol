88373e6d7b1c1b0dd8411905615fdc0e2cdb6dd1
Spahr Michael <spahr.michael@protonmail.com>
2022-03-14 16:13:50 +0100

    refactoring

--- contracts/ProjectToken.sol
+++ contracts/ProjectToken.sol
@@ -4,10 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/**
- * @title ProjectToken
- * @author Michael Spahr
- */
 contract ProjectToken is ERC20 {
     constructor() public ERC20("Project Token", "PJTK") {
         _mint(msg.sender, 1 * 10**6 * 10**18);
